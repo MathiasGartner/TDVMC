6cad9801d75d498b9c68c5e866577a1b98581239
gartner <gartner@asterix.tphys.uni-linz.ac.at>
2018-02-28 11:25:16 +0100

    initial files

---
+++ src/TDVMC.cpp
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : TDVMC.c
+ Author      : Mathias Gartner
+ Version     :
+ Copyright   : 
+ Description :
+ ============================================================================
+ */
+
+#include "mpi.h"
+#include <iostream>
+#include "test/PiCalculator.h"
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	int val = -1;
+	cout << "start" << endl;
+
+	if (argc > 1)
+	{
+		if (strcmp(argv[1], "-pitest") == 0)
+		{
+			CalculatePi(argc, argv);
+		}
+		else
+		{
+			//TODO: TDVMC
+		}
+	}
+
+	val = 0;
+	cout << "finish d " << val << endl;
+	return val;
+}
+
---
+++ src/test/PiCalculator.h
@@ -0,0 +1,49 @@
+/*
+ * PiCalculator.h
+ *
+ *  Created on: 27.02.2018
+ *      Author: Mathias Gartner
+ */
+#pragma once
+
+#include <math.h>
+#include "mpi.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void CalculatePi(int argc, char *argv[]) {
+	int rank, size;
+	double PI25DT = 3.141592653589793238462643;
+	double mypi, pi, x, y;
+	int total, inside;
+
+	MPI::Init(argc, argv);
+	size = MPI::COMM_WORLD.Get_size();
+	rank = MPI::COMM_WORLD.Get_rank();
+
+	total = 10000;
+	srand(rank + 1);
+
+	inside = 0;
+	for (int i = 0; i < total; i++)
+	{
+		x = ((double) rand() / RAND_MAX);
+		y = ((double) rand() / RAND_MAX);
+		if (sqrt(x*x + y*y) < 1.0)
+		{
+			inside++;
+		}
+	}
+	mypi = ((double)inside) / ((double)total) * 4.0;
+	cout << "this is process# " << rank << " mypi=" << mypi << endl;
+
+	MPI::COMM_WORLD.Reduce(&mypi, &pi, 1, MPI::DOUBLE, MPI::SUM, 0);
+	pi = pi / (double)size;
+	if (rank == 0)
+		cout << "pi is approximately " << pi << ", Error is "
+				<< fabs(pi - PI25DT) << endl;
+
+	MPI::Finalize();
+}
